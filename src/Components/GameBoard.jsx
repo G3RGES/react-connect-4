6195c812545b8fd2b6b9058a8960d658128f945b
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-02 17:02:17 +0200

    Update GameBoard.jsx

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -4,14 +4,30 @@ import GameCircle from "./GameCircle";
 const GameBoard = () => {
   return (
     <div>
-      <GameCircle id={1}>Red</GameCircle>
-      <GameCircle id={2}>Blue</GameCircle>
-      <GameCircle id={3}>Red</GameCircle>
-      <GameCircle id={4}>Blue</GameCircle>
-      <GameCircle id={5}>Red</GameCircle>
-      <GameCircle id={6}>Blue</GameCircle>
-      <GameCircle id={7}>Red</GameCircle>
-      <GameCircle id={8}>Blue</GameCircle>
+      <GameCircle id={1} color="red">
+        <span style={{ color: "red" }}>Red</span>
+      </GameCircle>
+      <GameCircle id={2} color="blue">
+        <span style={{ color: "blue" }}>Blue</span>
+      </GameCircle>
+      <GameCircle id={3} color="red">
+        <span style={{ color: "red" }}>Red</span>
+      </GameCircle>
+      <GameCircle id={4} color="blue">
+        <span style={{ color: "blue" }}>Blue</span>
+      </GameCircle>
+      <GameCircle id={5} color="red">
+        <span style={{ color: "red" }}>Red</span>
+      </GameCircle>
+      <GameCircle id={6} color="blue">
+        <span style={{ color: "blue" }}>Blue</span>
+      </GameCircle>
+      <GameCircle id={7} color="red">
+        <span style={{ color: "red" }}>Red</span>
+      </GameCircle>
+      <GameCircle id={8} color="blue">
+        <span style={{ color: "blue" }}>Blue</span>
+      </GameCircle>
     </div>
   );
 };
