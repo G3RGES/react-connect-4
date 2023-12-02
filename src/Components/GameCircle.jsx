31ddc389543066ee7956e361a82f70e0c39b0cb7
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-02 20:36:06 +0200

    Update GameCircle.jsx

--- src/Components/GameCircle.jsx
+++ src/Components/GameCircle.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 
 const GameCircle = ({ id, children, color }) => {
+  const style = {
+    backgroundColor: color,
+    width: 100,
+    height: 100,
+    margin: 10,
+    borderRadius: "50%",
+  };
+
   const clickHandler = () => {
     alert("u clicked" + id); //TESTING
   };
 
   return (
-    <div
-      onClick={clickHandler}
-      id={id}
-      style={{
-        backgroundColor: color,
-        width: 100,
-        height: 100,
-        margin: 10,
-        borderRadius: "50%",
-      }}
-    >
+    <div onClick={clickHandler} id={id} style={style}>
       {children}
     </div>
   );
