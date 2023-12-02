c9e8124efedce97cb40435e7f310ceb8f3ca3ed7
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-02 16:04:35 +0200

    Update GameCircle.jsx

--- src/Components/GameCircle.jsx
+++ src/Components/GameCircle.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const GameCircle = () => {
-  return <div>GameCircle</div>;
+const GameCircle = ({ id }) => {
+  const clickHandler = () => {
+    alert("u clicked" + id); //TESTING
+  };
+
+  return (
+    <div onClick={clickHandler} id={id}>
+      GameCircle - {id}
+    </div>
+  );
 };
 
 export default GameCircle;
