fea10831f95df56a558305fdc97e629da8ee564a
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-04 13:00:21 +0200

    Update App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import GameBoard from "./GameBoard";
-import Header from "./Header";
-import Footer from "./Footer";
 
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
       <GameBoard />
-      <Footer />
     </div>
   );
 };
