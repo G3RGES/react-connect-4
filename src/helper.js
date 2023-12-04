de0ec1075c34d16d3ce16fcde917ba488e942d16
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-04 19:12:20 +0200

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -28,3 +28,12 @@ export const isWinner = (gameBoard, currentMove, currentPlayer) => {
   }
   return false;
 };
+
+export const isDraw = (gameBoard, currentMove, currentPlayer) => {
+  let board = [...gameBoard];
+  board[currentMove] = currentPlayer;
+
+  let count = board.reduce((n, x) => n + (x === 0), 0);
+  // console.log(`count ${count}`); // TESTING
+  return count === 0;
+};
