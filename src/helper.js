833fa3f05d036e5ac085260aad62851dcdb98fc2
G3RGES <Georgios.Nashaat@gmail.com>
2023-12-05 20:00:58 +0200

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -37,3 +37,74 @@ export const isDraw = (gameBoard, currentMove, currentPlayer) => {
   // console.log(`count ${count}`); // TESTING
   return count === 0;
 };
+
+const randomComputerMove = (gameBoard) => {
+  let validMoves = [];
+  for (let i = 0; i < gameBoard.length; i++) {
+    if (gameBoard[i] === 0) {
+      validMoves.push(i);
+    }
+  }
+  let randomMove = Math.floor(Math.random() * validMoves.length);
+  return validMoves[randomMove];
+};
+
+export const computerMove = (gameBoard) => {
+  let moveChecks = [
+    // vertical
+    {
+      index: [0, 4, 8, 12],
+      max: 4,
+      step: 1,
+    },
+    // horozintal
+    {
+      index: [0, 1, 2, 3],
+      max: 16,
+      step: 4,
+    },
+    // diagonal
+    {
+      index: [0, 5, 10, 15],
+      max: 16,
+      step: 16,
+    },
+    // diagonal
+    {
+      index: [3, 6, 9, 12],
+      max: 16,
+      step: 16,
+    },
+  ];
+  let position = getPosition(gameBoard, moveChecks);
+  if (position > -1) return position;
+  return randomComputerMove(gameBoard);
+};
+
+const getPosition = (gameBoard, moveChecks) => {
+  for (let check = 0; check < moveChecks.length; check++) {
+    for (let i = 0; i < moveChecks[check].max; i += moveChecks[check].step) {
+      let series =
+        gameBoard[i + moveChecks[check].index[0]].toString() +
+        gameBoard[i + moveChecks[check].index[1]].toString() +
+        gameBoard[i + moveChecks[check].index[2]].toString() +
+        gameBoard[i + moveChecks[check].index[3]].toString();
+      switch (series) {
+        case "1110":
+        case "2220":
+          return i + moveChecks[check].index[3];
+        case "1101":
+        case "2202":
+          return i + moveChecks[check].index[2];
+        case "1011":
+        case "2022":
+          return i + moveChecks[check].index[1];
+        case "0111":
+        case "0222":
+          return i + moveChecks[check].index[0];
+        default:
+      }
+    }
+  }
+  return -1;
+};
